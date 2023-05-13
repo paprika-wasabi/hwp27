658319786cd15d4488b9f305cb459b81f58621d7
MarshallXXVI <Soonthornvech3@gmail.com>
2023-05-13 14:31:54 +0200

    update till ex2

--- Blatt3/ex1-7/ex1-7.ino
+++ Blatt3/ex1-7/ex1-7.ino
@@ -1,25 +1,32 @@
 
 long time;
+bool toggle1 = 0;
 
 void setup() {
-  delay(3000);
-  Serial.begin(2000000);
-  TCCR1A = 0;
-  TCCR1B = 0;
-
-  TCCR1A |= (1 << WGM12); // Turn on CTC mode.
-  TCCR1B |= (1 << CS10) | (1 << CS11) | (1 << CS12);
-  OCR1A = 255;
-  OCR1B = 255;
-
-  TIMSK1 |= (1 << OCIE1A);
-  TIMSK1 |= (1 << OCIE1B);
-
-  Serial.println("Setup done");
+  setTimer1Freq();
 }
 
 void setTimer1Freq(){
+   
+  //set pins as outputs
+  pinMode(12, OUTPUT);
 
+  cli();//stop interrupts
+
+  //set timer1 interrupt at 1Hz
+  TCCR1A = 0;// set entire TCCR1A register to 0
+  TCCR1B = 0;// same for TCCR1B
+  TCNT1  = 0;//initialize counter value to 0
+  // set compare match register for 1hz increments
+  OCR1A = 14;// = (16*10^6) / (1046*1024) - 1 (must be <65536)
+  // turn on CTC mode
+  TCCR1B |= (1 << WGM12);
+  // Set CS12 and CS10 bits for 1024 prescaler
+  TCCR1B |= (1 << CS12) | (1 << CS10);  
+  // enable timer compare interrupt
+  TIMSK1 |= (1 << OCIE1A);
+
+  sei();//allow interrupts
 }
 
 void setPin13(bool param) {
@@ -40,16 +47,16 @@ void Blink() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //Blink();
-
 }
 
-uint8_t counter = 0;
 ISR(TIMER1_COMPA_vect) {
-  counter++;
-  if (counter >= 30) {
-    counter = 0;
+  if (toggle1) {
+    //digitalWrite(13 , HIGH);
     PORTB |= (1 << 4);
+    toggle1 = 0;
+  } else {
+    //digitalWrite(13 , LOW);
+    PORTB &= ~(1 << 4);
+    toggle1 = 1;
   }
 }
