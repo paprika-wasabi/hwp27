0c52a190c23a7244d4dc2cc6e46c2e93617e5c2e
Whataduckinglife <52635142+Whataduckinglife@users.noreply.github.com>
2023-05-16 23:28:12 +0200

    Ex 1-5 Fully Functional, Ex6 requires work
    Ex6 resets the values due to some integere overflow(?), however, the code should work, at least in theory

--- Blatt3/ex1-7/ex1-7.ino
+++ Blatt3/ex1-7/ex1-7.ino
@@ -3,10 +3,15 @@
 
 long time;
 bool toggle1 = 0;
-volatile uint32_t tCount5 = 0;
+volatile uint32_t tCount5;
 uint16_t durations[10] = {2000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000};
 uint16_t notes[10] = {440, 494, 523, 587, 659, 698, 784, 880, 988, 1047};
-volatile uint8_t melodyIdx = 0;
+volatile uint8_t melodyIdx;
+volatile  uint8_t dur = 0;
+// new additions
+volatile uint8_t *timer2_pin_port;
+volatile uint8_t timer2_pin_mask;
+
 
 void debugprint(char* const msg) {
   if (DEBUG_MODE) {
@@ -40,23 +45,27 @@ void setTimer1Freq(int freq) {
     ________________________________________________________________
   */  
   cli(); // stop interrupts
-  // debugprint("SetTimer1");
+  debugprint("SetTimer1 in");
   // Set timer1 in CTC mode with TOP = OCR1A
   TCCR1A = 0;              // Clear TCCR1A register
   TCCR1B = (1 << WGM12);   // Set WGM12 bit for CTC mode
   TCCR1B |= (1 << CS10);   // Set prescaler to 1 (no prescaling)
-  uint16_t temp = (16e6) / (freq * 1024) - 1;
-  if (temp > ((uint16_t) pow(2, 16))) {
+  uint16_t ocr = (16e6) / (freq * 1024) - 1;
+  // uint32_t ocr = F_CPU / freq / 2 - 1;
+  
+  if (ocr > ((uint16_t) pow(2, 16))) {
+  // if (ocr > 0xffff) {
+    ocr = F_CPU / freq / 2 / 64 - 1;
     PORTB &= ~(1 << 4);
     TCCR1A = 0; // Clear TCCR1A register
     TCCR1B = 0; // Clear TCCR1B register
   }
-  OCR1A = temp;            // Set OCR1A value for desired frequency (1046 Hz)
+  OCR1A = ocr;            // Set OCR1A value for desired frequency (1046 Hz)
   // 14 = (16*10^6) / (1046*1024) - 1.
 
   // Toggle pin 10 on compare match
   TCCR1A |= (1 << COM1B0);
-
+  debugprint("SetTimer1 out");
   sei(); // allow interrupts
   
 
@@ -64,27 +73,74 @@ void setTimer1Freq(int freq) {
 
 void setTimer2(bool activate) {
   debugprint("SetTimer2");
+  
+  // if (activate) {
+  //   cli();
+  //   // Reset the initial drivers
+  //   TCCR2A = 0;
+  //   TCCR2B = 0;
+  //   TCCR2A = (1 << WGM21) | (1 << WGM20);
+  //   TCCR2B = (1 << WGM22) | (1 << CS20);
+  //   TCNT2 = 0;
+  //   OCR2A = 255;
+
+  //   TIMSK2 = 0;
+  //   TIMSK2 |= (1 << TOIE2);
+
+  //   sei();
+  // } else {
+  //   cli();
+  //   TCCR2A = 0;
+  //   TCCR2B = 0;
+  //   TIMSK2 = 0;
+  //   sei();
+  // }
   if (activate) {
     cli();
+    // Reset the initial drivers
+    TCCR2A = 0;
+    TCCR2B = 0;
+    // Enable CTC
     TCCR2A = (1 << WGM21) | (1 << WGM20);
     TCCR2B = (1 << WGM22) | (1 << CS20);
     TCNT2 = 0;
+    
+    // Set Frequency so that it interrupts every 1 ms
     OCR2A = 255;
-
+    timer2_pin_port = portOutputRegister(digitalPinToPort(10));
+    timer2_pin_mask = digitalPinToBitMask(10);
     TIMSK2 = 0;
     TIMSK2 |= (1 << TOIE2);
 
     sei();
   } else {
     cli();
-    TCCR2A = 0;
-    TCCR2B = 0;
-    TIMSK2 = 0;
+    // TCCR2A = 0;
+    // TCCR2B = 0;
+    // TIMSK2 = 0;
+    // finish interrupts
+    bitWrite(TIMSK2, OCIE2A, 0); // disable interrupt
+    TCCR2A = (1 << WGM20);
+    TCCR2B = (TCCR2B & 0b11111000) | (1 << CS22);
+    OCR2A = 0;
     sei();
   }
-
+  debugprint("SetTimer2 out");
 }
 
+/*
+ISR(TIMER1_COMPA_vect) {
+  if (toggle1) {
+    //digitalWrite(13 , HIGH);
+    PORTB |= (1 << 4);
+    toggle1 = 0;
+  } else {
+    //digitalWrite(13 , LOW);
+    PORTB &= ~(1 << 4);
+    toggle1 = 1;
+  }
+}
+*/
 
 void wait(unsigned long ms) {
   unsigned long now = millis();
@@ -92,24 +148,32 @@ void wait(unsigned long ms) {
 }
 
 ISR(TIMER2_OVF_vect) {
-  // debugprint("Interupt Routine in");
+  // debugprint("Interupt Routine in ");
   cli();
-  if (tCount5 >= durations[melodyIdx]) {
-    tCount5 = 0;
-    // Serial.print(tCount5);
-    // Serial.print("  ");
-    // Serial.println(melodyIdx);
-    // Serial.println();
+  if (durations[melodyIdx] > 0) {
+    dur = 2 * notes[melodyIdx] * durations[melodyIdx] / 1000;
+  } else {
+    dur = -1;
+  }
+  // dur = (8e6) / (durations[melodyIdx]) - 1;
+  Serial.print(dur);
+  Serial.print(" ");
+  Serial.print(melodyIdx);
+  Serial.print(" ");
+  Serial.println(tCount5);
+  if (tCount5 >= dur) {
     melodyIdx++;
-    wait(10);
-
+    tCount5 = 0;
   }
   tCount5++;
+  Serial.println(melodyIdx);
   sei();
-  // Serial.println(melodyIdx);
-  debugprint("Interupt Routine out");
+  
+  
 }
+uint32_t calculateDuration() {
 
+}
 
 void setPin13(bool param) {
   if (param) {
@@ -145,33 +209,46 @@ void playMelody() {
   */
   pinMode(10, OUTPUT);
   Serial.println(melodyIdx);
+  setTimer2(true);
+  tCount5 = 0;
   
+  Serial.println(tCount5);
   while (melodyIdx < 10) {
+    dur = (8e6) / (durations[melodyIdx]) - 1;
     setTimer2(true);
     Serial.println(notes[melodyIdx]);
     setTimer1Freq(notes[melodyIdx]);
-    delay(durations[melodyIdx]);
-    delay(10); // Optional delay between notes
+    Serial.println();
+    // wait(durations[melodyIdx]);
+    // wait(10); // Optional delay between notes
     setTimer2(false);
   }
-  
+  setTimer2(false);
+  Serial.println(melodyIdx);
+  setTimer1Freq(0);
 }
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(38400);
+  melodyIdx = 0;
+
+  //Serial.println(melodyIdx);
+  
   // debugprint("Setup start");
   pinMode(10, OUTPUT);
-  // playMelody();
+  playMelody();
   // debugprint("Setup end");
-  
-  Serial.begin(9600);
+  // tone(10, 440, 100);
   
   // debugprint("Timer1");
-  setTimer1Freq(notes[melodyIdx]);
+  // setTimer1Freq(notes[melodyIdx]);
   // debugprint("Timer 2");
  
   setTimer2(true);
   // debugprint("Timer2 out");
 }
 
-void loop() {}
+void loop() {
+  Serial.println(tCount5);
+  wait(1000);
+}
